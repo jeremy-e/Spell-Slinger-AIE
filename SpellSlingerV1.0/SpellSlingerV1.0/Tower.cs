682643a8f84163281cda9cbdd8746920a53d5b23
Adam Hulbert <hulbert.adam@gmail.com>
2014-08-11 18:00:27 +1000

    Type names changed to capps

--- SpellSlingerV1.0/SpellSlingerV1.0/Tower.cs
+++ SpellSlingerV1.0/SpellSlingerV1.0/Tower.cs
@@ -13,7 +13,7 @@ namespace SpellSlingerV1._0
             this.SpriteID = "tower.png";
             this.Width = 64;
             this.Height = 64;
-            this.Type = (int)TYPE.tower;
+            this.Type = (int)TYPE.TOWER;
             this.X = Game1.SCREEN_WIDTH / 2 - 32;
             this.Y = Game1.SCREEN_HEIGHT/2 - 32;
         }
