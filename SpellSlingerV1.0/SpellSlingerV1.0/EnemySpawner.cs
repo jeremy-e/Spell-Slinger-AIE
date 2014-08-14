9c1036b516add593efb6824ec32f5d1014109e9c
Adam Hulbert <hulbert.adam@gmail.com>
2014-08-14 18:56:17 +1000

    Added EnemySpawner Class (empty)

---
+++ SpellSlingerV1.0/SpellSlingerV1.0/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpellSlingerV1._0
+{
+    class EnemySpawner
+    {
+
+
+        public EnemySpawner()
+        {
+
+        }
+    }
+}
