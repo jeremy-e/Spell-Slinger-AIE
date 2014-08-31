78f5458ca02c60131d38ffcb4fa85792bab94ecc
jeremy-e <jez@outbackpixels.com.au>
2014-08-31 15:41:39 +0800

    awsum spell graphic add

--- SpellSlingerV1.0/SpellSlingerV1.0/SpriteManager.cs
+++ SpellSlingerV1.0/SpellSlingerV1.0/SpriteManager.cs
@@ -36,12 +36,12 @@ namespace SpellSlingerV1._0
         private string[] enemySpriteFileNames = new string[enemyNumTextures];
         private string[] spellSpriteFileNames = new string[spellNumTextures];
         private string[] GUISpriteFileNames = new string[GUINumTextures];
-        
+
         ///List Textures here
         public SpriteManager()
         {
             playerSpriteFileNames[(int)PLAYER_SPRITES.TOWER] = "tower.png";
-            
+
             enemySpriteFileNames[(int)ENEMY_TYPE.GHOUL] = "enemy1.png";
             enemySpriteFileNames[(int)ENEMY_TYPE.RUNNING_GHOUL] = "enemy2.png";
             enemySpriteFileNames[(int)ENEMY_TYPE.HEAVY_ZOMBIE] = "enemy3.png";
@@ -50,14 +50,12 @@ namespace SpellSlingerV1._0
             enemySpriteFileNames[(int)ENEMY_TYPE.WEREWOLF] = "enemy6.png";
             enemySpriteFileNames[(int)ENEMY_TYPE.GREEN_DRAGON] = "enemy7.png";
 
-            spellSpriteFileNames[(int)SPELL_TYPE.FIREBALL] = "spell1.png";
-            spellSpriteFileNames[(int)SPELL_TYPE.ICELANCE] = "spell2.png";
-            
             //TODO - ADD Different sprites for these spells. 
-            //I Added these so I could do all the resistance/weakness stuff, we dont have to use them for the demo. 
-            spellSpriteFileNames[(int)SPELL_TYPE.DESPAIR] = "spell1.png";
+            spellSpriteFileNames[(int)SPELL_TYPE.FIREBALL] = "spell1.png";
+            spellSpriteFileNames[(int)SPELL_TYPE.ICELANCE] = "spell3.png";
+            spellSpriteFileNames[(int)SPELL_TYPE.DESPAIR] = "spell4.png";
             spellSpriteFileNames[(int)SPELL_TYPE.LIGHTNING] = "spell2.png";
-            spellSpriteFileNames[(int)SPELL_TYPE.RAPTURE] = "spell1.png";
+            spellSpriteFileNames[(int)SPELL_TYPE.RAPTURE] = "spell5.png";
             ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
